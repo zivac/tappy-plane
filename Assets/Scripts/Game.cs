489fe2e94af87b04bf81b4ce003634f3a050b13e
Darko Zivkovic <darko.zivkovic@gauss.hr>
2021-05-06 20:25:50 +0200

    demo structure

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
